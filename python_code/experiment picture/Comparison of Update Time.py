62039ec3f24c1cc6e462290a277ec251603349fc
zhangqian <1037140492@qq.com>
2024-05-29 21:35:24 +0800

    修改图片样式

--- python_code/experiment picture/Comparison of Update Time.py
+++ python_code/experiment picture/Comparison of Update Time.py
@@ -6,19 +6,20 @@ values = [0.203, (150+350)/2, 2.3, 20, 15, 128]  # Converting all to seconds for
 special_index = 3  # Index for the special '>20s' case
 
 # Creating a bar chart
-plt.figure(figsize=(12, 6))
-bars = plt.bar(categories, values, color=['skyblue' if i != special_index else 'orange' for i in range(len(values))])
+plt.figure(figsize=(10, 10))
+bars = plt.bar(categories, values, color=['#E67E22' if i != special_index else 'orange' for i in range(len(values))],width=0.6)
 
 # Adding values on top of the bars, with a special label for the '>20s' case
 for bar, val, cat in zip(bars, values, categories):
     yval = bar.get_height()
     label = f'>{val}s' if cat == 'Wang et al.' else f'{val}s'
-    plt.text(bar.get_x() + bar.get_width()/2, yval, label, ha='center', va='bottom')
+    plt.text(bar.get_x() + bar.get_width()/2, yval, label, ha='center', va='bottom',fontsize=20)
 
-plt.xlabel('Related Work')
-plt.ylabel('Update Time (s)')
-plt.title('Comparison of Update Time')
-plt.xticks(rotation=45, ha='right')
+
+plt.ylabel('Update Time (s)',fontsize=20)
+# plt.title('Comparison of Update Time')
+plt.xticks(rotation=45, ha='right', fontsize=14)
+plt.yticks(fontsize=16)
 plt.grid(axis='y', linestyle='--', alpha=0.7)
 
 # Show the plot
